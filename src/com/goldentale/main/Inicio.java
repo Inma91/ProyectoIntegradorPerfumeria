27f44abc5f7a4441bcbe4650d4447f4d60020b05
Inmaculada Gil <inma0810@gmail.com>
2026-05-25 22:47:02 +0200

    Documento Properties

--- src/com/goldentale/main/Inicio.java
+++ src/com/goldentale/main/Inicio.java
@@ -1,9 +1,25 @@
 package com.goldentale.main;
 
+import java.awt.EventQueue;
+
 public class Inicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				//Vista General
+				//VPrincipal vp = new VPrincipal(); 
+				
+				//Controlador c = new Controlador (vp, ); 
+				
+				//vp.setControlador(c); 
+				
+				//vp.hacerVisible(); 
+				
+			}
+		});
 
 	}
 
