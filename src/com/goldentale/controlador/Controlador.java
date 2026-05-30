c6999f2214a355ac05dc94829baf39840a1c34db
Inmaculada Gil <inma0810@gmail.com>
2026-05-30 17:31:07 +0200

    Creación de Aceso a BD Prop Creación de Vista Principal Modificación de Inicio, Controlador, LogIn

--- src/com/goldentale/controlador/Controlador.java
+++ src/com/goldentale/controlador/Controlador.java
@@ -1,5 +1,34 @@
 package com.goldentale.controlador;
 
-public class Controlador {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import com.goldentale.vistaEmpleado.LoginVista;
+import com.goldentale.vistaEmpleado.VistaPrincipal;
+
+public class Controlador implements ActionListener {
+
+	private VistaPrincipal vp; 
+	private LoginVista lgv;
+	
+	// Crear atributos que representen los paneles
+	//private PRealizarEncuesta pre;
+
+	public Controlador(VistaPrincipal vp, LoginVista lgv) {
+		this.vp = vp;
+		this.lgv = lgv;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() instanceof JButton) {
+			if (e.getActionCommand().equals(VistaPrincipal.INGRESAR_EN_APLICACION)) {
+			vp.cargarPanel(lgv);
+			}
+		}
+		
+	}
 
 }
--- src/com/goldentale/main/Inicio.java
+++ src/com/goldentale/main/Inicio.java
@@ -2,6 +2,10 @@ package com.goldentale.main;
 
 import java.awt.EventQueue;
 
+import com.goldentale.controlador.Controlador;
+import com.goldentale.vistaEmpleado.LoginVista;
+import com.goldentale.vistaEmpleado.VistaPrincipal;
+
 public class Inicio {
 
 	public static void main(String[] args) {
@@ -9,14 +13,43 @@ public class Inicio {
 			
 			@Override
 			public void run() {
-				//Vista General
-				//VPrincipal vp = new VPrincipal(); 
+				//Vista de Bienvenida a la aplicación
+				VistaPrincipal vp = new VistaPrincipal(); 
 				
-				//Controlador c = new Controlador (vp, ); 
+				//Vista con el login de cliente/empleado
+				LoginVista lgv = new LoginVista(); 
 				
-				//vp.setControlador(c); 
+				//Vista del empleado con las opciones que puede manejar
+				//EmpleadoDashboardVista empdv = new EmpleadoDashboardVista();
 				
-				//vp.hacerVisible(); 
+				//Vista para que el empleado añada un nuevo perfume
+				//AniadirPerfumeVista apv = new AniadirPerfumeVista(); 
+				
+				//Vista para que el empleado modifique los datos de algún perfume
+				//ModificarPerfumeVista mpv = new ModificarPerfumeVista();
+				
+				//Vista para que el empleado consulte el número de existencias
+				//StockVista sv = new StockVista();
+				
+				//Vista para que el empleado gestione los pedidos/vea el estado de los pedidos
+				//GestionarPedidosVista gpv = new GestionarPedidosVista();
+
+				//Vista del cliente con las opciones que puede manejar
+				//ClienteDashboardVista clidv = new ClienteDashboardVista();
+				
+				//Controlador c = new Controlador (vp, lgv, empdv, apv, mpv, sv, gvp);
+				Controlador c = new Controlador (vp, lgv);
+				
+				vp.setControlador(c); 
+				lgv.setControlador(c); 
+				//empdv.setControlador(c);
+				//apv.setControlador(c); 
+				//mpv.setControlador(c);
+				//sv.setControlador(c);
+				//gpv.setControlador(c);
+				//clidv.setControlador(c);
+				
+				vp.hacerVisible(); 
 				
 			}
 		});
